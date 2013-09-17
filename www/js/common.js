179bc1aaec008d43198bdfc03afd1ac96e2967b8
Summeli <summeli@summeli.fi>
2013-09-17 17:23:50 +0300

    added a new error page

--- www/js/common.js
+++ www/js/common.js
@@ -1,1 +1,2 @@
 var g_clientId="4537ddd0";
+var radioListPageLoaded=false;
--- www/js/radioListPage.js
+++ www/js/radioListPage.js
@@ -1,18 +1,33 @@
-
-$( document ).delegate("#radioListPage", "pageinit", function() {
-   
+function refreshRadioListData() {
+   $.mobile.loading( 'show',{text:'loading'} );
    var jqxhr = $.getJSON('http://api.jamendo.com/v3.0/radios?client_id='+g_clientId+'&callback=?', null, null,'application/json')
    .done(function(result) {
+      radioListPageLoaded=true;
       console.log(result);
+      $.mobile.loading( 'hide' );
 
-      
       var list = $("#radioList").listview();
       $(result.results).each(function(index){
-         $(list).append('<li><a href="index.html#radioPage" data-transition="slide" onclick="sessionStorage.radioId=' + this.id+ '">'    
+         $(list).append('<li><a href="index.html#radioPage" data-transition="slide" onclick="sessionStorage.radioId=' + this.id+ '">'
            + '<img src="'+this.image+'" class="ui-li-icon"/> <h3>' + this.dispname + '</h3></a> </li>');
       });
 
       $("#radioList").listview('refresh');
    })
-   .fail(function() { console.log( "error" ); });
+   .fail(function() {
+     radioListPageLoaded = false;
+     jQuery.mobile.changePage("#errorPage");
+     });
+}
+
+$( document ).delegate("#radioListPage", "pageinit", function() {
+   refreshRadioListData();
+});
+
+$( document ).delegate("#radioListPage", "pagebeforeshow", function()  {
+   if(radioListPageLoaded == true)
+      return;
+
+   //page is not loaded continue
+   refreshRadioListData();
 });
